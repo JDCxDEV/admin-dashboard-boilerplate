0b75ee39996af075fe534c4918c94ea736ad4dfb
jeremy delacruz <jeremy.delacruz.dev@gmail.com>
2024-10-22 05:39:56 +0800

    add admin structure

--- resources/js/Pages/Dashboard.tsx
+++ resources/js/Pages/Dashboard.tsx
@@ -1,4 +1,4 @@
-import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
+import AuthenticatedLayout from '@/Layouts/AuthenticatedSidebarLayout';
 import { Head } from '@inertiajs/react';
 import { PageProps } from '@/types';
 
